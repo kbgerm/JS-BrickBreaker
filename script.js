20493bdfda1cad59f5d09522d527cd1ad13ee559
kb_germ <k.b.germ1806@gmail.com>
2024-10-06 17:02:17 +0200

    feat: ball direction and walls hard

--- script.js
+++ script.js
@@ -1,46 +1,65 @@
 const config = {
-    squares: [
-      {
-        position: [0, 25],
-        size: [60, 25],
-        color: 'rgb(180, 180, 180)',
-        hardness: undefined,
-      },
-      {
-        position: [60, 25],
-        size: [60, 25],
-        color: 'rgb(238, 130, 238)',
-        hardness: 1,
-      },
-      {
-        position: [120, 25],
-        size: [60, 25],
-        color: 'rgb(0, 0, 255)',
-        hardness: 2,
-      }, 
-      {
-        position: [180, 25],
-        size: [60, 25],
-        color: 'rgb(255, 165, 0)',
-        hardness: 3,
-      },
-    ],
-    ball : {
-      position: [60, 300],
-      size: [10, 10],
-      color: 'rgb(106, 90, 205)',
+  squares: [
+    {
+      position: [0, 25],
+      size: [60, 25],
+      color: 'rgb(180, 180, 180)',
+      hardness: undefined,
     },
-    platform: {
-      position: [145, 350],
-      size: [60, 10],
-      color: 'rgb(60, 60, 60)',
-    }
-  };
+    {
+      position: [60, 25],
+      size: [60, 25],
+      color: 'rgb(238, 130, 238)',
+      hardness: 1,
+    },
+    {
+      position: [120, 25],
+      size: [60, 25],
+      color: 'rgb(0, 0, 255)',
+      hardness: 2,
+    },
+    {
+      position: [180, 25],
+      size: [60, 25],
+      color: 'rgb(255, 165, 0)',
+      hardness: 3,
+    },
+    {
+      position: [240, 25],
+      size: [60, 25],
+      color: 'rgb(238, 130, 238)',
+      hardness: 3,
+    },
+    {
+      position: [300, 25],
+      size: [60, 25],
+      color: 'rgb(255, 165, 0)',
+      hardness: 3,
+    },
+  ],
+  ball: {
+    position: [150, 300],
+    size: [10, 10],
+    direction: [-1, -1],
+    color: 'rgb(106, 90, 205)',
+  },
+  platform: {
+    position: [145, 370],
+    size: [60, 10],
+    color: 'rgb(60, 60, 60)',
+  },
+  wall: {
+    right: [350, 0, 350, 400],
+    left: [0, 0, 0, 400],
+    top: [0, 0, 350, 0],
+    bottom: [0, 400, 350, 400],
+  },
+};
 
 const canvas = document.querySelector('#canvas');
 
 document.addEventListener('DOMContentLoaded', draw);
-function draw () {
+function draw() {
   const ctx = canvas.getContext("2d");
 
   // скорбар
@@ -54,14 +73,65 @@ function draw () {
   // отрисовка квадратов
   for (let i = 0; i < config.squares.length; i++) {
     ctx.fillStyle = config.squares[i].color;
-    ctx.fillRect(...config.squares[i].position,...config.squares[i].size);
+    ctx.fillRect(...config.squares[i].position, ...config.squares[i].size);
   }
 
   // шарик
   ctx.fillStyle = config.ball.color;
-  ctx.fillRect(...config.ball.position,...config.ball.size);
+  ctx.fillRect(...config.ball.position, ...config.ball.size);
 
   // платформа
   ctx.fillStyle = config.platform.color;
-  ctx.fillRect(...config.platform.position,...config.platform.size);
+  ctx.fillRect(...config.platform.position, ...config.platform.size);
 }
+
+// function pointOnCircle(center, radius, angle) {
+//   // return [
+//   //   center[0] + Math.floor(radius*Math.cos(angle)),
+//   //   center[1] + Math.floor(radius*Math.sin(angle)),
+//   // ];
+// }
+
+function moveBall() {
+  const ctx = canvas.getContext("2d");
+  ctx.clearRect(...config.ball.position, ...config.ball.size);
+
+  const ball = config.ball;
+  ball.position[0] += ball.direction[0];
+  ball.position[1] += ball.direction[1];
+
+  const wall = config.wall;
+  if (ball.position[0] < wall.left[0]) {
+    ball.direction[0] = -ball.direction[0];
+  }
+
+  if (ball.position[1] < wall.top[1]) {
+    ball.direction[1] = -ball.direction[1];
+  }
+
+  if ((ball.position[0] + ball.size[0]) > wall.right[0]) {
+    ball.direction[0] = -ball.direction[0];
+  }
+
+  if ((ball.position[1] + ball.size[1]) > wall.bottom[1]) {
+    ball.direction[1] = -ball.direction[1];
+  }
+
+  const platform = config.platform;
+  if ((ball.position[1] + ball.size[1]) > platform.position[1] 
+  && ball.position[0] + ball.size[0] > platform.position[0] 
+  && ball.position[0] + ball.size[0] < platform.position[0] + platform.size[0]) {
+    ball.direction[1] = -ball.direction[1];
+  }
+
+  ctx.fillStyle = config.ball.color;
+  ctx.fillRect(...config.ball.position, ...config.ball.size);
+
+  requestAnimationFrame(draw);
+}
+
+setInterval(moveBall, 1)
+
+// остановка около стены(любой)
+//       145   205
+// +++++++-----+++++++
